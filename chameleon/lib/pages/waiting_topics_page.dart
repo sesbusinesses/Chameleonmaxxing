e4bc95743da95cf43b788259a68c461ada24ae83
Ethan Dancho <ethan@dancho.org>
2024-02-21 20:37:49 -0700

    Fixed stupid  sytax error

--- chameleon/lib/pages/waiting_topics_page.dart
+++ chameleon/lib/pages/waiting_topics_page.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import '../widgets/selectable_grid.dart';
-import '../widgets/wide_button.dart';
 
 class WaitingTopicsPage extends StatelessWidget {
   const WaitingTopicsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          const SelectableGrid(
+          SelectableGrid(
             displayList: ['topic1', 'topic2', 'topic3', 'topic4'], // Example usernames
             color: Colors.grey,
           ),
-          const SizedBox(height: 40),
+          SizedBox(height: 40),
           // Padding(
           //   padding: const EdgeInsets.only(bottom: 40),
           //   child: WideButton(
