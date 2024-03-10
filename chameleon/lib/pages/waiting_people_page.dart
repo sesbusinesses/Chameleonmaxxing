39bb3437fd663cf079063b4eb91c466af0dc100f
SungMin Park <spark259@asu.edu>
2024-03-10 12:06:58 -0700

    Playernames diplaygrid sync in real time now, voting status doesn't sync yet

--- chameleon/lib/pages/waiting_people_page.dart
+++ chameleon/lib/pages/waiting_people_page.dart
@@ -22,93 +22,100 @@ class WaitingPeoplePage extends StatefulWidget {
 }
 
 class _WaitingPeoplePageState extends State<WaitingPeoplePage> {
-  late Future<List<String>> playerNamesFuture;
+  late Stream<List<String>> playerNamesStream;
   late Future<List<bool>> hasVotedFuture;
 
   @override
   void initState() {
     super.initState();
-    playerNamesFuture = DatabaseManager.getPlayerUsernames(widget.roomCode);
+    playerNamesStream = DatabaseManager.streamPlayerUsernames(widget.roomCode);
     hasVotedFuture = DatabaseManager.getVotingTopicStatus(widget.roomCode);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<dynamic>>(
-        future: Future.wait([playerNamesFuture, hasVotedFuture]),
+      body: StreamBuilder<List<String>>(
+        stream: playerNamesStream,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const CircularProgressIndicator();
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           } else if (snapshot.hasData) {
-            final List<String> playerNames = snapshot.data![0];
-            final List<bool> votingStatus = snapshot.data![1];
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                const Text(
-                  'Waiting for Players',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                Expanded(
-                  child: DisplayGrid(
-                    userList: playerNames,
-                    hasVoted: votingStatus,
-                  ),
-                ),
-                if (widget.isHost)
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 15),
-                    child: WideButton(
-                      text: 'Start Game',
-                      onPressed: () {
-                        // Use an anonymous async function to await the startGame call
-                        () async {
-                          await DatabaseManager.startGame(widget.roomCode);
-                          // After starting the game, navigate to the GamePage
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => GamePage(
-                                roomCode: widget.roomCode,
-                                playerId: widget.playerId,
-                              ),
-                            ),
-                          );
-                        }();
-                      },
-                    ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 40),
-                  child: WideButton(
-                    text: 'Leave Game',
-                    color: Colors.red,
-                    onPressed: () async {
-                      // Use a local variable for context to avoid using BuildContext across async gaps
-                      var localContext = context;
-                      try {
-                        if (widget.isHost) {
-                          await DatabaseManager.removeEntireRoom(
-                              widget.roomCode);
-                        } else {
-                          await DatabaseManager.removePlayerFromRoom(
-                              widget.roomCode, widget.playerId);
-                        }
-                        Navigator.pop(localContext); // Use localContext here
-                      } catch (e) {
-                        showMessage(localContext,
-                            'Error leaving the game. Please try again.'); // Use localContext here
-                      }
-                    },
-                  ),
-                ),
-              ],
+            final List<String> playerNames = snapshot.data!;
+            return FutureBuilder<List<bool>>(
+              future: hasVotedFuture,
+              builder: (context, hasVotedSnapshot) {
+                if (hasVotedSnapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (hasVotedSnapshot.hasError) {
+                  return Text('Error: ${hasVotedSnapshot.error}');
+                } else if (hasVotedSnapshot.hasData) {
+                  final List<bool> votingStatus = hasVotedSnapshot.data!;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      const Text(
+                        'Waiting for Players',
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      Expanded(
+                        child: DisplayGrid(
+                          userList: playerNames,
+                          hasVoted: votingStatus,
+                        ),
+                      ),
+                      if (widget.isHost)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: WideButton(
+                            text: 'Start Game',
+                            onPressed: () {
+                              () async {
+                                await DatabaseManager.startGame(widget.roomCode);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => GamePage(
+                                      roomCode: widget.roomCode,
+                                      playerId: widget.playerId,
+                                    ),
+                                  ),
+                                );
+                              }();
+                            },
+                          ),
+                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 40),
+                        child: WideButton(
+                          text: 'Leave Game',
+                          color: Colors.red,
+                          onPressed: () async {
+                            var localContext = context;
+                            try {
+                              if (widget.isHost) {
+                                await DatabaseManager.removeEntireRoom(widget.roomCode);
+                              } else {
+                                await DatabaseManager.removePlayerFromRoom(widget.roomCode, widget.playerId);
+                              }
+                              Navigator.pop(localContext);
+                            } catch (e) {
+                              showMessage(localContext, 'Error leaving the game. Please try again.');
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  return const Text('No voting status available');
+                }
+              },
             );
           } else {
-            return const Text('No data available');
+            return const Text('No player data available');
           }
         },
       ),
