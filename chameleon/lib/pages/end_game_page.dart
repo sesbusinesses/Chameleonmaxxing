652da61782b29f35a4be595bb47c827473ce806f
SungMin Park <spark259@asu.edu>
2024-03-17 18:13:18 -0700

    people get kicked out if the host leaves the room after game.

--- chameleon/lib/pages/end_game_page.dart
+++ chameleon/lib/pages/end_game_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'play_again_page.dart'; // Make sure to import your PlayAgainPage
 import 'leaderboard_page.dart'; // Make sure to import your LeaderboardPage
+import '../models/database_manager.dart';
+import '../widgets/utility.dart';
+import 'dart:async';
 
 class EndGamePage extends StatefulWidget {
   final String roomCode;
@@ -16,7 +19,52 @@ class EndGamePage extends StatefulWidget {
   _EndGamePageState createState() => _EndGamePageState();
 }
 
-class _EndGamePageState extends State<EndGamePage> {
+class _EndGamePageState extends State<EndGamePage> {  
+  //late Stream<bool> gameRunningStream;
+  //late StreamSubscription<bool> gameRunningSubscription;
+  late Stream<bool> doesRoomExistStream;
+  late StreamSubscription<bool> doesRoomExistSubscription;
+  
+  @override
+  void initState() {
+    super.initState();
+    
+    //gameRunningStream = DatabaseManager.streamGameRunning(widget.roomCode);
+    doesRoomExistStream = DatabaseManager.streamDoesRoomExist(widget.roomCode);
+
+    /*
+    gameRunningSubscription = gameRunningStream.listen((isGameRunning) {
+      if (isGameRunning) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => GamePage(
+              roomCode: widget.roomCode,
+              playerId: widget.playerId,
+            ),
+          ),
+        );
+      }
+    });
+    */
+
+    doesRoomExistStream = DatabaseManager.streamDoesRoomExist(widget.roomCode);
+    doesRoomExistSubscription = doesRoomExistStream.listen((doesGameExist) {
+      if (!doesGameExist) {
+        Navigator.popUntil(context, (route) => route.isFirst);
+        showMessageWarning(context, 'The room no longer exists.');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    //gameRunningSubscription.cancel();
+    doesRoomExistSubscription.cancel();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
