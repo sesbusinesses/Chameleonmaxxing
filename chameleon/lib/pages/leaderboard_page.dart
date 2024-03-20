73af2e476f19e63a6e95d97248dabfb4232fc859
Ethan Dancho <ethan@dancho.org>
2024-03-20 16:04:23 -0700

    leaderboard shits

--- chameleon/lib/pages/leaderboard_page.dart
+++ chameleon/lib/pages/leaderboard_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/database_manager.dart';
-import '../models/player_score.dart'; // Ensure this path matches your project structure
+import '../models/player_score.dart'; // Make sure this path matches your project structure
 
 class LeaderboardPage extends StatefulWidget {
   final String roomCode;
@@ -28,45 +28,65 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<PlayerScore>>(
-        future: leaderboardScores,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error.toString()}'));
-          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            return ListView.separated(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                PlayerScore score = snapshot.data![index];
-                Color? backgroundColor; // Optional background color
-                if (index == 0) { // First place
-                  backgroundColor = Color(0xFFffb703);
-                } else if (index == 1) { // Second place
-                  backgroundColor = Color(0xFFccc5b9);
-                } else if (index == 2) { // Third place
-                  backgroundColor = Color(0xFFd4a373);
-                }
-
-                return Container(
-                  color: backgroundColor,
-                  child: ListTile(
-                    leading: Text('#${index + 1}',
-                        style: const TextStyle(fontSize: 16)),
-                    title: Text(score.playerName,
-                        style: const TextStyle(fontSize: 16)),
-                    trailing: Text('${score.score} pts',
-                        style: const TextStyle(fontSize: 16)),
+      body: SafeArea(
+        child: FutureBuilder<List<PlayerScore>>(
+          future: leaderboardScores,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error.toString()}'));
+            } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: MediaQuery.of(context).size.height -
+                        (Scaffold.of(context).appBarMaxHeight ?? 0),
                   ),
-                );
-              },
-              separatorBuilder: (context, index) => const Divider(),
-            );
-          } else {
-            return const Center(child: Text('No data available'));
-          }
-        },
+                  child: IntrinsicHeight(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ...snapshot.data!.asMap().entries.map<Widget>((entry) {
+                          int index = entry.key;
+                          PlayerScore score = entry.value;
+                          Color? backgroundColor;
+                          if (index == 0) {
+                            backgroundColor = Color(0xFFffb703);
+                          } else if (index == 1) {
+                            backgroundColor = Color(0xFFccc5b9);
+                          } else if (index == 2) {
+                            backgroundColor = Color(0xFFd4a373);
+                          }
+                          List<Widget> widgets = [
+                            Container(
+                              color: backgroundColor,
+                              child: ListTile(
+                                leading: Text('#${index + 1}',
+                                    style: const TextStyle(fontSize: 16)),
+                                title: Text(score.playerName,
+                                    style: const TextStyle(fontSize: 16)),
+                                trailing: Text('${score.score} pts',
+                                    style: const TextStyle(fontSize: 16)),
+                              ),
+                            ),
+                          ];
+                          // Check if this is not the last entry, then add a Divider
+                          if (index != snapshot.data!.length - 1) {
+                            widgets.add(const Divider());
+                          }
+                          return Column(children: widgets);
+                        }).toList(),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            } else {
+              return const Center(child: Text('No data available'));
+            }
+          },
+        ),
       ),
     );
   }
